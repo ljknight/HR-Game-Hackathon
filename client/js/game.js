d8388a860c3d06ab1371df68dd00ecbd943b93ae
Laura Knight <ljknight@gmail.com>
2015-09-16 21:20:42 -0700

    Created simple pizza-eating llama game

--- client/js/game.js
+++ client/js/game.js
@@ -1,53 +1,91 @@
-var game = new Phaser.Game(800, 600, Phaser.AUTO, 'gameContainer');
+var game = new Phaser.Game(1000, 600, Phaser.AUTO, 'gameContainer');
 
+var cursors;
 var mainState = {
   preload: function () {
-    game.stage.backgroundColor = '#666';
-    game.load.image('player', 'assets/player.png'); 
-    game.load.image('ground', 'assets/ground.png');
+    game.stage.backgroundColor = '#00CBFF';
+    game.load.image('player', 'assets/llama.png'); 
+    // game.load.image('pizza', 'assets/pizza.png'); 
+    game.load.spritesheet('pizza', 'assets/pizza.png', 0, 0);
+    // game.load.image('ground', 'assets/ground.png');
   },
   create: function () {
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
     this.player = this.game.add.sprite(100, 245, 'player');
     game.physics.arcade.enable(this.player);
-    this.player.body.gravity.y = 1000; 
-    this.player.body.bounce.y = 0.5;
+    // this.player.body.gravity.y = 1000; 
+    // this.player.body.bounce.y = 0.5;
     this.player.body.collideWorldBounds = true;
-    
-    this.platforms = game.add.group();
-    this.platforms.enableBody = true;
-    
-    this.ground = [];
-    for (var i = 0; i < game.world.width; i+=70) {
-      this.ground.push(this.platforms.create(i, game.world.height - 70, 'ground'));
+
+    // this.pizza = this.game.add.sprite(10, 10, 'pizza');
+    // game.physics.arcade.enable(this.pizza);
+    // this.pizza.body.gravity.y = 1000; 
+    // this.pizza.body.bounce.y = 0.5;
+    // this.pizza.body.collideWorldBounds = true;
+
+    group = game.add.group();
+    group.enableBody = true;
+    group.physicsBodyType = Phaser.Physics.ARCADE;
+
+
+    for (var i = 0; i < 40; i++) {
+      var c = group.create(game.rnd.integerInRange(0, 900), game.rnd.integerInRange(0, 500), 'pizza', 17);
+      c.name = 'pizza' + i;
+      c.body.immovable = true;
     }
-    for (var j = 0; j < this.ground.length; j++) {
-      this.ground[j].body.immovable = true;
-    }
+    
+    // this.ground = [];
+    // for (var i = 0; i < game.world.width; i+=70) {
+    //   this.ground.push(this.platforms.create(i, game.world.height - 70, 'ground'));
+    // }
+    // for (var j = 0; j < this.ground.length; j++) {
+    //   this.ground[j].body.immovable = true; 
+    // }
+    cursors = game.input.keyboard.createCursorKeys();
+
     
   },
-  update: function () {
-    for (var i = 0; i < this.ground.length; i++) {
-      game.physics.arcade.collide(this.player, this.ground[i]);
-    }
-    this.player.body.velocity.x = 0;
 
-    var cursors = game.input.keyboard.createCursorKeys();
+  update: function () {
+    // for (var i = 0; i < this.ground.length; i++) {
+    //   game.physics.arcade.collide(this.player, this.ground[i]); 
+    // }
+    game.physics.arcade.overlap(this.player, group, collisionHandler, null, this);
+
+    this.player.body.velocity.x = 0;
+    this.player.body.velocity.y = 0;
+
     if (cursors.left.isDown)
     {
-        this.player.body.velocity.x = -150;
+        this.player.body.velocity.x = -200;
     }
     else if (cursors.right.isDown)
     {
-        this.player.body.velocity.x = 150;
+        this.player.body.velocity.x = 200;
     }
-    if (cursors.up.isDown && this.player.body.touching.down)
+
+    if (cursors.up.isDown)
     {
-        this.player.body.velocity.y = -600;
+        this.player.body.velocity.y = -200;
+    }
+    else if (cursors.down.isDown)
+    {
+        this.player.body.velocity.y = 200;
     }
   }
 };
+
+function collisionHandler (player, pizza) {
+    //  If the player collides with the pizza then they get eaten
+    //  The pizza frame ID is 17
+
+    if (pizza.frame == 17)
+    {
+        pizza.kill();
+    }
+
+}
 
 game.state.add('main', mainState);
 game.state.start('main');
